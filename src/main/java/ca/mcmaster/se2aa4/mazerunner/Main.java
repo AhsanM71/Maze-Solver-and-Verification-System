55a9e54dcc90a3b5d456e769ba0f1e13b04543e1
AhsanM71 <ahsanmuzammil71@gmail.com>
2024-01-12 12:47:41 -0500

    updated file path problem

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -13,14 +13,21 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Main {
+    public static String path;
 
     private static final Logger logger = LogManager.getLogger();
 
     public static void main(String[] args) {
         logger.info("** Starting Maze Runner");
+        Options options = new Options();
+        options.addOption("i", true, "flag type");
+        options.addOption("path", true, "file path");
+        CommandLineParser parser = new DefaultParser();
         try {
-            logger.info("**** Reading the maze from file " + args[0] + " " + args[1]);
-            BufferedReader reader = new BufferedReader(new FileReader(args[1]));
+            CommandLine cmd = parser.parse(options, args);
+            path = cmd.getOptionValue("i", "i");
+            logger.info("**** Reading the maze from file " + args[0] + " " + path);
+            BufferedReader reader = new BufferedReader(new FileReader(path));
             String line;
             while ((line = reader.readLine()) != null) {
                 for (int idx = 0; idx < line.length(); idx++) {
